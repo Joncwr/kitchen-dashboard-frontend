2a0a6c0b12f8812de485704beff1d8b9fdc68317
Jonathan <joncwrrr@gmail.com>
2019-01-03 19:17:38 +0800

    no message

---
+++ src/components/Ordering/OrderComponent/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import './index.css'
+
+class OrderComponent extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+
+    }
+
+  }
+
+  renderOrders() {
+
+  }
+
+  render() {
+    return (
+      <div className="orderComponent">
+      </div>
+    )
+  }
+}
+
+export default OrderComponent
---
+++ src/components/Ordering/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import OrderComponent from './OrderComponent'
+
+import './index.css'
+
+class Ordering extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+
+    }
+
+  }
+
+  renderOrders() {
+    let dataArr = testData
+    if (dataArr.length > 0) {
+      let renderDataArr = []
+      dataArr.map((data,index) => {
+        renderDataArr.push(
+          <OrderComponent
+            key={index}
+            data={data}
+          />
+        )
+      })
+
+      return (
+        <div className="ordering">
+          {renderDataArr}
+        </div>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className="default">
+        {this.renderOrders()}
+      </div>
+    )
+  }
+}
+
+export default Ordering
+
+
+let testData = [
+  'Jon', 'Chuchu'
+]
