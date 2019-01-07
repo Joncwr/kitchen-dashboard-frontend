363100b3a73ffb17801ef9b4a2d5bcb3169086fa
Jonathan <joncwrrr@gmail.com>
2019-01-07 20:22:51 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Route, Router } from 'react-router-dom'
 import createHistory from 'history/createBrowserHistory'
 
+import socket from './services/Sockets.js'
+import Accounts from './services/Accounts.js'
+
 import Start from './Start'
 import Ordering from './components/Ordering'
 import RecipeViewer from './components/RecipeViewer'
@@ -15,9 +18,68 @@ class App extends Component {
     super()
 
     this.state = {
+      data: [],
     }
   }
-  
+
+  componentDidMount() {
+    if (localStorage.getItem('data')) {
+      let data = localStorage.getItem('data')
+      this.setState({data: JSON.parse(data)})
+    }
+    else {
+      Accounts.accountData()
+      .then(res => {
+        let dataArr = []
+        res.forEach(data => {
+          dataArr.push({name: data, orders: []})
+        })
+
+        this.setState({data: dataArr},() => {
+          localStorage.setItem('data', JSON.stringify(dataArr))
+        })
+      })
+      .catch(err => console.log(err))
+    }
+
+    socket.on('socket_order', res => {
+      Accounts.authorizeUser(res.name)
+      .then(() => {
+        this.changeOrders(res, 'add')
+      })
+      .catch(err => console.log(err))
+    });
+
+    socket.on('delete_last_order', res => {
+      Accounts.authorizeUser(res.name)
+      .then(() => {
+        this.changeOrders(res, 'delete')
+      })
+      .catch(err => console.log(err))
+    })
+  }
+
+  changeOrders(res, method) {
+    this.state.data.forEach((data, index) => {
+      if (data.name === res.name) {
+        let data = Object.assign([], this.state.data)
+        let dataObject = Object.assign({},this.state.data[index])
+
+        if (method === 'add') {
+          dataObject.orders.push(res.order)
+        }
+        else if (method === 'delete') {
+          dataObject.orders.pop()
+        }
+        data[index] = dataObject
+
+        this.setState({data: data},() => {
+          localStorage.setItem('data', JSON.stringify(data))
+        })
+      }
+    })
+  }
+
   render() {
     return (
       <Router history={history}>
@@ -31,6 +93,7 @@ class App extends Component {
           <Route exact path="/ordering" render={() => (
             <Ordering
               history={history}
+              data={this.state.data}
             />
           )} />
           <Route exact path="/recipeviewer" render={() => (
--- src/components/Ordering/index.js
+++ src/components/Ordering/index.js
@@ -8,78 +8,8 @@ import UsersComponent from './UsersComponent'
 import './index.css'
 
 class Ordering extends React.Component {
-  constructor(){
-    super()
-
-    this.state = {
-      data: [],
-    }
-
-  }
-
-  componentDidMount() {
-    if (localStorage.getItem('data')) {
-      let data = localStorage.getItem('data')
-      this.setState({data: JSON.parse(data)},() => {
-
-      this.calcOrders()
-      })
-    }
-    else {
-      Accounts.accountData()
-      .then(res => {
-        let dataArr = []
-        res.forEach(data => {
-          dataArr.push({name: data, orders: []})
-        })
-
-        this.setState({data: dataArr},() => {
-          localStorage.setItem('data', JSON.stringify(dataArr))
-        })
-      })
-      .catch(err => console.log(err))
-    }
-
-    socket.on('socket_order', res => {
-      Accounts.authorizeUser(res.name)
-      .then(() => {
-        this.changeOrders(res, 'add')
-      })
-      .catch(err => console.log(err))
-    });
-
-    socket.on('delete_last_order', res => {
-      Accounts.authorizeUser(res.name)
-      .then(() => {
-        this.changeOrders(res, 'delete')
-      })
-      .catch(err => console.log(err))
-    })
-  }
-
-  changeOrders(res, method) {
-    this.state.data.forEach((data, index) => {
-      if (data.name === res.name) {
-        let data = Object.assign([], this.state.data)
-        let dataObject = Object.assign({},this.state.data[index])
-
-        if (method === 'add') {
-          dataObject.orders.push(res.order)
-        }
-        else if (method === 'delete') {
-          dataObject.orders.pop()
-        }
-        data[index] = dataObject
-
-        this.setState({data: data},() => {
-          localStorage.setItem('data', JSON.stringify(data))
-        })
-      }
-    })
-  }
-
   renderUsers() {
-    let dataArr = Object.assign([],this.state.data)
+    let dataArr = Object.assign([],this.props.data)
     if (dataArr.length > 0) {
       let renderDataArr = []
       dataArr.forEach((data,index) => {
@@ -105,7 +35,7 @@ class Ordering extends React.Component {
 
   calcOrders() {
     let orders = 0
-    let dataArr = Object.assign([],this.state.data)
+    let dataArr = Object.assign([],this.props.data)
 
     dataArr.forEach(data => {
       data.orders.forEach(orderData => {
