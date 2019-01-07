e7b79ba8edd086840dc88022191de068e89f21a8
Joncwr <joncwrrr@gmail.com>
2019-01-07 12:11:02 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Router } from 'react-router-dom'
 import createHistory from 'history/createBrowserHistory'
 
+import socketIOClient from "socket.io-client";
 import Start from './Start'
 import Ordering from './components/Ordering'
 import RecipeViewer from './components/RecipeViewer'
@@ -9,8 +10,22 @@ import RecipeViewer from './components/RecipeViewer'
 import './App.css';
 
 const history = createHistory()
+const socketEndpoint = 'http://localhost:8000'
+const socket = socketIOClient(socketEndpoint);
+
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+    }
+  }
+
+  componentDidMount() {
+    socket.on('timer', res => console.log(res));
+  }
+
   render() {
     return (
       <Router history={history}>
